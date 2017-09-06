2aad86409e3ff7a4da5dbe0ffb0b7a2ef89ac728
Matias Käkelä <matias.kakela@relex.fi>
2017-09-06 08:35:10 +0300

    Move to scaleway

--- src/emitter.js
+++ src/emitter.js
@@ -2,9 +2,13 @@ import dotenv from 'dotenv';
 import ruuvi from 'node-ruuvitag';
 import DB from './db';
 
-dotenv.config({ path: './influxdb.env' });
+dotenv.config();
+
+console.log("Loading database");
 
 const influx = DB.instance();
+
+console.log("Starting...");
 
 ruuvi.on('found', tag => {
   console.log('Found RuuviTag, id: ' + tag.id);
